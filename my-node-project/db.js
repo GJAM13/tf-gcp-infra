0802a922d9c9eeac9843ce566c6bc2c293e72be6
JAPAN GOR <japan.gor@logicauto.io>
2024-02-14 02:50:52 -0500

    Update db.js

--- my-node-project/db.js
+++ my-node-project/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
-  database: process.env.DB_NAME || 'mydb'
+  database: process.env.DB_NAME || 'web_app'
 });
 
 connection.connect((err) => {
