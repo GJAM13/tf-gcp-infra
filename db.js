b0793ed6a433568d2550f699f6916552282cf137
Japan Gor <gor.j@northeastern.edu>
2024-02-21 17:31:23 -0500

    Update db.js

--- db.js
+++ db.js
@@ -10,7 +10,6 @@ connection.connect((err) => {
   if (err) {
     process.exit(1); // Exit the process with an error code
   }
-  console.log('Connected to the database');
 });
 
 module.exports = connection; // Export the connection
